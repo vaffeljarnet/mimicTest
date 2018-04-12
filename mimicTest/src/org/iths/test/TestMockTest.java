a26d94837a2f090235b46944d5cbbcc82b64aa23
Johan <studiotumult@gmail.com>
2018-04-12 20:16:56 +0200

    Created first JUnit test

--- mimicTest/src/org/iths/test/TestMockTest.java
+++ mimicTest/src/org/iths/test/TestMockTest.java
@@ -2,13 +2,17 @@ package org.iths.test;
 
 import static org.junit.Assert.*;
 
+import org.iths.main.MockTest;
 import org.junit.Test;
 
 public class TestMockTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		MockTest mock = new MockTest();
+		mock.storeQuestionAndAnswer("1+1", "2");
+		String answer = mock.checkQuestion("1+1");
+		assertEquals("2",answer);
 	}
 
 }
