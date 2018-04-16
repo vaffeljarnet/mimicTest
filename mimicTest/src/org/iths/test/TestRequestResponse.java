052fd35e1c621f498d849d5e6400697ba8d76f0a
larss_000 <larss_000@LAPTOP-1A0S11AO>
2018-04-16 10:49:56 +0200

    Added testcase 1

--- mimicTest/src/org/iths/test/TestRequestResponse.java
+++ mimicTest/src/org/iths/test/TestRequestResponse.java
@@ -1,14 +1,19 @@
 package org.iths.test;
 
 import static org.junit.Assert.*;
+import org.iths.main.RequestResponse;
 
+import org.iths.main.Store;
 import org.junit.Test;
 
 public class TestRequestResponse {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Store mock = new Store();
+		mock.learnResponse("1+1", "2");
+		String answer = mock.getResponse("1+1");
+		assertEquals("2",answer);
 	}
 
 }
