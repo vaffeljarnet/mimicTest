837c8ba9dfee962e69ed774df5b4847400b56c1c
larss_000 <larss_000@LAPTOP-1A0S11AO>
2018-04-16 10:57:53 +0200

    Added two valid and invalid null testcase

--- mimicTest/src/org/iths/test/TestRequestResponse.java
+++ mimicTest/src/org/iths/test/TestRequestResponse.java
@@ -15,5 +15,21 @@ public class TestRequestResponse {
 		String answer = mock.getResponse("1+1");
 		assertEquals("2",answer);
 	}
+	
+	@Test
+	public void validNullRequest() {
+		Store mock = new Store();
+		mock.learnResponse("1+1", null);
+		String answer = mock.getResponse("1+1");
+		assertEquals(null,answer);
+	}
+	
+	@Test
+	public void invalidNullRequest() {
+		Store mock = new Store();
+		mock.learnResponse(null, null);
+		String answer = mock.getResponse(null);
+		assertEquals(null,answer);
+	}
 
 }
