a5fb7db040d147550272914ac1f4e733f381da7b
Johan <studiotumult@gmail.com>
2018-04-16 16:48:14 +0200

    Changed name of test file and added ID's to test cases.

--- mimicTest/src/org/iths/test/TestStore.java
+++ mimicTest/src/org/iths/test/TestStore.java
@@ -6,11 +6,11 @@ import org.iths.main.RequestResponse;
 import org.iths.main.Store;
 import org.junit.Test;
 
-public class TestRequestResponse {
+public class TestStore {
 	//TODO Add test case ID
 	
 	@Test
-	public void test() {
+	public void ID111ValidValues() {
 		Store mock = new Store();
 		mock.learnResponse("1+1", "2");
 		String answer = mock.getResponse("1+1");
@@ -18,7 +18,7 @@ public class TestRequestResponse {
 	}
 	
 	@Test
-	public void validNullRequest1() {
+	public void ID112ValidNullRequest() {
 		Store mock = new Store();
 		mock.learnResponse("1+1", null);
 		String answer = mock.getResponse("1+1");
@@ -26,11 +26,13 @@ public class TestRequestResponse {
 	}
 	
 	@Test
-	public void invalidNullRequest() {
+	public void ID113InvalidNullRequest() {
 		Store mock = new Store();
 		mock.learnResponse(null, "2");
 		String answer = mock.getResponse(null);
 		assertEquals("2",answer);
 	}
+	
+	
 
 }
