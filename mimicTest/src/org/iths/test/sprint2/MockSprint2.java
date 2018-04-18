21db789021091a93b988b9058ff6a7d75fe39d79
carol <carol@LAPTOP-NAOPN4BE>
2018-04-18 17:31:21 +0200

    added code for chromedriver

--- mimicTest/src/org/iths/test/sprint2/MockSprint2.java
+++ mimicTest/src/org/iths/test/sprint2/MockSprint2.java
@@ -2,4 +2,8 @@ package org.iths.test.sprint2;
 
 public class MockSprint2 {
 
+//	private WebDriver webdriver;
+//	System.setProperty("webdriver.chrome.driver", "chromedrive/chromedriver.exe");
+//	webdriver = new ChromeDriver();
+	
 }
