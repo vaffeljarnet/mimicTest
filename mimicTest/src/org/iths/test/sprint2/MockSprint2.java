6354389c9894520728d9317a19d3505add780216
larss_000 <larss_000@LAPTOP-1A0S11AO>
2018-04-19 09:36:35 +0200

    Changed name of file.

--- mimicTest/src/org/iths/test/sprint2/MockSprint2.java
+++ mimicTest/src/org/iths/test/sprint2/MockSprint2.java
@@ -1,6 +1,6 @@
 package org.iths.test.sprint2;
 
-public class MockSprint2 {
+public class TestSelenium {
 
 //	private WebDriver webdriver;
 //	System.setProperty("webdriver.chrome.driver", "chromedrive/chromedriver.exe");
