204d3e94c1d44c5048c7e835a6b8d9c26dd38f34
larss_000 <larss_000@LAPTOP-1A0S11AO>
2018-04-19 11:06:45 +0200

    Added comments to TestMockTestSprint2.java

--- mimicTest/src/org/iths/test/sprint2/TestMockTestSprint2.java
+++ mimicTest/src/org/iths/test/sprint2/TestMockTestSprint2.java
@@ -5,22 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMockTestSprint2 {
-
+	
+	/**
+	 * Test for storing a request with response and answer directly
+	 */
 	@Test
 	public void test() {
 		MockTestSprint2 mock = new MockTestSprint2();
-		String response = mock.sendGetRequest("localhost:8080/hej?storeRequest=då");
+		String response = mock.sendGetRequest("localhost:8080/1+1?storeRequest=2");
 		System.out.println(response);
 	}
 	
+	/**
+	 * Test for storing a request with the wrong answer and then relearn it.
+	 */
 	@Test
 	public void test2() {
 		MockTestSprint2 mock = new MockTestSprint2();
-		String test = mock.sendGetRequest("localhost:8080/hej?storeRequest=då");
+		String test = mock.sendGetRequest("localhost:8080/1+1?storeRequest=3");
 		String response = mock.sendGetRequest("localhost:8080/hej?learn");
 		System.out.println(response);
 	}
 	
+	/**
+	 * Test for storing a request and then checking that we can ask that question.
+	 */
 	@Test
 	public void test3() {
 		MockTestSprint2 mock = new MockTestSprint2();
