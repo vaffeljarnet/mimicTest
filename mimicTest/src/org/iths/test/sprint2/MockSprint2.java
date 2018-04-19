3aed9735496dc11bb0efbea8753d0cd29bf552be
larss_000 <larss_000@LAPTOP-1A0S11AO>
2018-04-19 09:39:45 +0200

    Changed files in test directory for sprint2

--- mimicTest/src/org/iths/test/sprint2/MockSprint2.java
+++
@@ -1,9 +0,0 @@
-package org.iths.test.sprint2;
-
-public class TestSelenium {
-
-//	private WebDriver webdriver;
-//	System.setProperty("webdriver.chrome.driver", "chromedrive/chromedriver.exe");
-//	webdriver = new ChromeDriver();
-	
-}
