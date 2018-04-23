a81dd36a94b54a1f1239279bbc7adeeea080013e
carol <carol@LAPTOP-NAOPN4BE>
2018-04-23 15:48:45 +0200

    update

--- mimicTest/src/org/iths/test/sprint2/SeleniumWorkingTest.java
+++ mimicTest/src/org/iths/test/sprint2/SeleniumWorkingTest.java
@@ -10,10 +10,11 @@ public class SeleniumWorkingTest {
 		TestSelenium driver = new TestSelenium();
 		driver.openURL("http://localhost:8080/2+2"); 
 		driver.sendText("4");
+		driver.delay(2000);
 		driver.clickLearn();
+		driver.delay(2000);
 		driver.openURL("http://localhost:8080/2+2");
 		Assert.assertEquals("4", driver.getValue());
-		driver.delay(2000);
 		driver.quitSelenium();
 	}	  
 }
