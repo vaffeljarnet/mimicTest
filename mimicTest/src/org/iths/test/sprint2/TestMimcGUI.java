1b1fd09f4ad6478eb32981171badedfe182b04bc
larss_000 <larss_000@LAPTOP-1A0S11AO.lan>
2018-04-24 13:02:45 +0200

    Updated test cases with host variable.

--- mimicTest/src/org/iths/test/sprint2/TestMimcGUI.java
+++ mimicTest/src/org/iths/test/sprint2/TestMimcGUI.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 public class TestMimcGUI {
 	
 	String host = "http://localhost:8080/";
-	String request;
 	
 //	@Test
 	public void learnReqResp() {
@@ -15,8 +14,7 @@ public class TestMimcGUI {
 //		maybe put unlearn in the last steps in the testcase?
 		
 		TestSelenium driver = new TestSelenium();
-		request = "2+2";
-		driver.openURL(host+request); 
+		driver.openURL(host+"2+2"); 
 		driver.sendText("4");
 		driver.clickLearn();
 		driver.openURL("http://localhost:8080/2+2");
@@ -30,11 +28,11 @@ public class TestMimcGUI {
 //		This I need help with
 		
 		TestSelenium driver = new TestSelenium();
-		driver.openURL("http://localhost:8080/1+1"); 
+		driver.openURL(host+"1+1"); 
 		driver.sendText("3");
 		driver.clickLearn();
-		driver.openURL("http://localhost:8080/LearnNextResponse?text=2");
-		driver.openURL("http://localhost:8080/1+1");
+		driver.openURL(host+"LearnNextResponse?text=2");
+		driver.openURL(host+"1+1");
 		Assert.assertEquals("2", driver.getValue());
 		driver.quitSelenium();
 	}	
@@ -45,11 +43,11 @@ public class TestMimcGUI {
 //		shows "nothing to unlearn" if you don't add line 46?!
 		
 		TestSelenium driver = new TestSelenium();
-		driver.openURL("http://localhost:8080/5+5"); 
+		driver.openURL(host+"5+5"); 
 		driver.sendText("10");
 		driver.clickLearn();
 		//driver.openURL("http://localhost:8080/5+5");
-		driver.openURL("http://localhost:8080/unlearn");
+		driver.openURL(host+"unlearn");
 		Assert.assertEquals("OK", driver.getValue());
 		driver.quitSelenium();
 	}
@@ -60,7 +58,7 @@ public class TestMimcGUI {
 //		This test passed
 		
 		TestSelenium driver = new TestSelenium();
-		driver.openURL("http://localhost:8080/KillMimic"); 
+		driver.openURL(host+"KillMimic"); 
 		Assert.assertEquals("OK", driver.getValue());
 		driver.quitSelenium();
 	}	
