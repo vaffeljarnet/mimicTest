ed52e400e0e4bacb67df5d21b9bd777ccad8a185
Marina Jevdjenic <marina.jevdjenic@gmail.com>
2018-04-25 09:07:44 +0200

    Sprint 2 manual

--- mimicTest/src/org/iths/test/sprint2/TestMimcGUI.java
+++ mimicTest/src/org/iths/test/sprint2/TestMimcGUI.java
@@ -1,129 +1,86 @@
 package org.iths.test.sprint2;
 
-import static org.junit.Assert.fail;
-
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMimcGUI {
-
-	private String host = "http://localhost:8080/";
-	private MimicJarHelper helper = new MimicJarHelper();
-	private MimicGuiSelenium driver;
-
-	/**
-	 * Makes a request for a not stored request, uses the
-	 * response form to set a response and requests again
-	 * to check that the response has been stored.
-	 */
-	@Test
-	public void TestMimicGUI111learnReqResp() {
-		if(helper.jarIsRunning()) {
-
-			driver = new MimicGuiSelenium();
-			driver.openURL(host+"2+2");
-			driver.sendText("4");
-			driver.clickLearn();
-			driver.openURL(host+"2+2");
-			Assert.assertEquals("4", driver.getValue());
-			driver.openURL(host+"unlearn");
-
-		}else {
-			fail(helper.errorString());
-		}
-	}	
 	
-	/**
-	 * Stores a request with wrong response using the 
-	 * response form, relearns that same request and checks
-	 * that the new response is stored for the request. 
-	 */
+	String host = "http://localhost:8080/";
+	
+    @Test
+	public void TestMimicGUI111learnReqResp() {
+		
+//		this test passed, need to "unlearn" first if this is to be run more than one time
+//		maybe put unlearn in the last steps in the testcase?
+		
+		MimicGuiSelenium driver = new MimicGuiSelenium();
+		driver.openURL(host+"2+2");
+		driver.sendText("4");
+		driver.clickLearn();
+		driver.openURL(host+"2+2");
+		Assert.assertEquals("4", driver.getValue());
+		driver.openURL(host+"unlearn");
+		driver.quitSelenium();
+	}	
+
 	@Test
 	public void TestMimicGUI112changeResp() {
-		if(helper.jarIsRunning()) {
-			driver = new MimicGuiSelenium();
-			driver.openURL(host+"1+1"); 
-			driver.sendText("3");
-			driver.clickLearn();
-			driver.openURL(host+"1+1");
-			Assert.assertEquals("3", driver.getValue());
-			driver.openURL(host+"LearnNextResponse?text=2");
-			driver.openURL(host+"1+1");
-			Assert.assertEquals("2", driver.getValue());
-			driver.openURL(host+"unlearn");
-
-		}else {
-			fail(helper.errorString());
-		}
+		
+//		This I need help with
+		
+		MimicGuiSelenium driver = new MimicGuiSelenium();
+		driver.openURL(host+"1+1"); 
+		driver.sendText("3");
+		driver.clickLearn();
+		driver.openURL(host+"1+1");
+		Assert.assertEquals("3", driver.getValue());
+		driver.openURL(host+"LearnNextResponse?text=2");
+		driver.openURL(host+"1+1");
+		Assert.assertEquals("2", driver.getValue());
+		driver.openURL(host+"unlearn");
+		driver.quitSelenium();
 	}	
-
-	/**
-	 * Stores a request using the response form and
-	 * immediately tries to unlearn it. 
-	 */
+	
 	@Test
 	public void TestMimicGUI113unlearnWithoutRequest() {
-		if(helper.jarIsRunning()) {
-
-			driver = new MimicGuiSelenium();
-			driver.openURL(host+"5+5"); 
-			driver.sendText("10");
-			driver.clickLearn();
-			driver.openURL(host+"unlearn");
-			Assert.assertEquals("OK", driver.getValue());
-			driver.openURL(host+"5+5"); 
-			driver.openURL(host+"unlearn");
-
-		}else {
-			fail(helper.errorString());
-		}
-	}
-	
-	/**
-	 * Stores a request using the response form, makes
-	 * that sam request again and then unlearns it.
-	 */
-	@Test
-	public void TestMimicGUI114unlearnWithRequest() {
-		if(helper.jarIsRunning()) {
-
-			driver = new MimicGuiSelenium();
-			driver.openURL(host+"5+5"); 
-			driver.sendText("10");
-			driver.clickLearn();
-			driver.openURL(host+"5+5"); 
-			driver.openURL(host+"unlearn");
-			Assert.assertEquals("OK", driver.getValue());
-
-		}else {
-			fail(helper.errorString());
-		}
-	}
-	
-	/**
-	 * Stores a request with xml format using the response
-	 * form and checks that the request has been identified 
-	 * as xml.
-	 */
-	@Test
-	public void TestMimicGUI115storeXmlRequest() {
-		if(helper.jarIsRunning()) {
-
-			driver = new MimicGuiSelenium();
-			driver.openURL(host+"xml"); 
-			driver.sendText("<value>1</value>");
-			driver.clickLearn();
-			driver.openURL(host+"xml"); 
-			Assert.assertTrue(driver.getValue().contains("This XML file"));
-
-		}else {
-			fail(helper.errorString());
-		}
-	}
-	
-	@After
-	public void teardown() {
+		
+//		shows "nothing to unlearn" if you don't add line 46?!
+		
+		MimicGuiSelenium driver = new MimicGuiSelenium();
+		driver.openURL(host+"5+5"); 
+		driver.sendText("10");
+		driver.clickLearn();
+		driver.openURL(host+"unlearn");
+		Assert.assertEquals("OK", driver.getValue());
+		driver.openURL(host+"5+5"); 
+		driver.openURL(host+"unlearn");
 		driver.quitSelenium();
 	}
+	
+	@Test
+	public void TestMimicGUI114unlearnWithRequest() {
+		
+//		shows "nothing to unlearn" if you don't add line 46?!
+		
+		MimicGuiSelenium driver = new MimicGuiSelenium();
+		driver.openURL(host+"5+5"); 
+		driver.sendText("10");
+		driver.clickLearn();
+		driver.openURL("http://localhost:8080/5+5");
+		driver.openURL(host+"unlearn");
+		Assert.assertEquals("OK", driver.getValue());
+		driver.quitSelenium();
+	}
+	
+	
+//	@Test
+	public void TestMimicGUI115closeMimic() {
+		
+//		This test passed
+		
+		MimicGuiSelenium driver = new MimicGuiSelenium();
+		driver.openURL(host+"KillMimic"); 
+		Assert.assertEquals("OK", driver.getValue());
+		driver.quitSelenium();
+	}	
 }
