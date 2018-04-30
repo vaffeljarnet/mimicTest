efd36a618b2e3ab8795bf0b67e8aeb3c2cd352a5
Johan <studiotumult@gmail.com>
2018-04-30 20:17:05 +0200

    Created legacy package for old versions of classes.

---
+++ mimicTest/src/org/iths/legacy/MimicJarHelper1.java
@@ -0,0 +1,57 @@
+package org.iths.legacy;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+public class MimicJarHelper1 {
+
+	private HttpServiceCaller1 service;
+	private final static String host="http://localhost:8080/"; 
+	
+	public boolean jarIsRunning() {
+		service = new HttpServiceCaller1();
+		if(!service.executeGetRequest(host).equals("Error")) {
+			resetMimic();
+			return true;
+		}else if(service.executeGetRequest(host).equals("Error")) {
+			startMimic();
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void resetMimic() {
+		killMimic();
+		wait(100);
+		startMimic();
+		wait(100);
+	}
+	
+	public void startMimic() {
+		try {
+			Desktop.getDesktop().open(new File("commonFiles\\runnableJars\\currentRelease\\mimic.jar"));
+		} catch (IOException e) {
+			System.err.println("Please start mimic.jar manually before running tests again. Location in repository: \\commonFiles\\runnableJars\\releaseSprint2\\mimic.jar");
+		}
+	}
+	
+	public void killMimic() {
+		service = new HttpServiceCaller1();
+		service.executeGetRequest("http://localhost:8080/KillMimic");
+		wait(100);
+	}
+	
+	public String errorString() {
+		return "Please start mimic.jar manually before running tests again. Location in repository: \\commonFiles\\runnableJars\\releaseSprint2\\mimic.jar";
+	}
+	
+	public void wait(int milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
